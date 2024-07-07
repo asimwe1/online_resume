8bd6e8b38d629506b467273e5832218868f2a5f5
Asimwe Landry <landryasimwe@gmail.com>
2024-07-07 15:08:56 +0200

    google script

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,14 +5,20 @@ import Recommendations from '../components/HomeComponents/Recommendations/Recomm
 import ClientReviews from '../components/HomeComponents/ClientReviews/ClientReviews';
 const home = () => {
     return (
-        <div className="Home-Page -z-10">
-            <Banner />
-            <MyExpertise />
-            <Recommendations />
-            <ClientReviews />
-            <Footer />
+        <main>
+            <head>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6172208143098274"
+                    crossorigin="anonymous"></script>
+            </head>
+            <div className="Home-Page -z-10">
+                <Banner />
+                <MyExpertise />
+                <Recommendations />
+                <ClientReviews />
+                <Footer />
+            </div>
 
-        </div>
+        </main>
     )
 }
 
